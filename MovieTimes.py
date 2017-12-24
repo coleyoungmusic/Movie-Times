526d63b7314e71525fd8ec01761f80ae5995e1c6
Micheal Cole Young <coleyoungmusic@gmail.com>
2017-12-23 18:27:50 -0600

    Update MovieTimes.py

--- MovieTimes.py
+++ MovieTimes.py
@@ -1,22 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
-
+import pprint
 # Movie titles and times are subject to Cinemark Allen 16 and XD ONLY
 
 url = "https://www.cinemark.com/north-texas/cinemark-allen-16-and-xd"
 r = requests.get(url)
 
-soup = BeautifulSoup(r.content, "html.parser")
+soup = BeautifulSoup(r.text, "html.parser")
 
-movie_title = soup.find_all("h2")
+headers = soup.select('h2') # selects all movie titles
 
-titles = [movie.text for movie in movie_title]
+movieTitles = [] # list that will contain h2 elements
 
-print(titles)
+for x in headers:
+    movieTitles.append(x.text) # movieTitles now contains list of titles
 
-digitalCinema = soup.find_all("div", {"class": "showtimeMovieBlock"})
+for x in movieTitles:
+    print(x)
 
-movieTime = [time.text.strip() for time in digitalCinema]
-movieTimes = ''.join(movieTime)
+#titleChoice = list(input("Enter what movie you would like to see: ").split())
 
-print(movieTimes, sep=" ", end="", flush=True)
+showtime_divs = soup.find_all("div", {"class": "col-xs-12 col-sm-10"})
+for x in showtime_divs:
+    movieTimes = []
+    movieTimes.append(x)
+    movieTimes2 = ''.join(x.text).split()
+    movietimes3 = [' '.join(movieTimes2)]
+    pprint.pprint(movietimes3)
